41dd8aa2a52afed2ea7639bbf458a2bf132fd850
Oscar Otero <oom@oscarotero.com>
2025-09-16 18:17:54 +0200

    typography

--- _config.ts
+++ _config.ts
@@ -1,9 +1,16 @@
 import lume from "lume/mod.ts";
 import basePath from "lume/plugins/base_path.ts";
+import lightningcss from "lume/plugins/lightningcss.ts";
+import googleFonts from "lume/plugins/google_fonts.ts";
 
-const site = lume();
-
-site.use(basePath())
+const site = lume()
+  .use(basePath())
+  .use(googleFonts({
+    fonts:
+      "https://fonts.google.com/share?selection.family=Geist:wght@100..900",
+  }))
+  .use(lightningcss())
+  .add("style.css")
   .add("img");
 
 export default site;
