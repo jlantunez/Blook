f8cb67d6ae48603349b56597116a963d8efd06a2
Oscar Otero <oom@oscarotero.com>
2025-09-19 13:49:33 +0200

    preparar cms

--- _cms.ts
+++ _cms.ts
@@ -2,6 +2,17 @@ import lumeCMS from "lume/cms/mod.ts";
 
 const cms = lumeCMS();
 
+// user and pass are environment variables, stored in an .env file
+const user = Deno.env.get("CMS_USER") ?? "admin";
+const pass = Deno.env.get("CMS_PASSWORD") ?? "";
+
+cms.auth({
+  [user]: pass,
+});
+
+// Enable git to pull/push changes
+cms.git();
+
 cms.collection({
   name: "Posts",
   store: "src:posts/*.md",
